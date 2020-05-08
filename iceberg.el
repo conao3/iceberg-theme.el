e4525c77c46f7d0fb569fbd1e98d15a60adefc96
conao3 <conao3@gmail.com>
2020-05-09 02:27:07 +0900

    generate project

---
+++ iceberg.el
@@ -0,0 +1,43 @@
+;;; iceberg.el --- Well-designed, eye-friendly, dark blue color scheme  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/iceberg.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Well-designed, eye-friendly, dark blue color scheme.
+
+
+;;; Code:
+
+(defgroup iceberg nil
+  "Well-designed, eye-friendly, dark blue color scheme."
+  :prefix "iceberg-"
+  :group 'tools
+  :link '(url-link :tag "Github" "https://github.com/conao3/iceberg.el"))
+
+(provide 'iceberg)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; iceberg.el ends here
