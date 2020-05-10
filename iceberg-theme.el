046f75dbe28b1549261b721cc313d40841702adf
conao3 <conao3@gmail.com>
2020-05-10 14:50:10 +0900

    add sample config

--- iceberg-theme.el
+++ iceberg-theme.el
@@ -25,6 +25,14 @@
 
 ;; Well-designed, eye-friendly, dark blue color scheme.
 
+;; Sample configure
+
+;;   (leaf iceberg-theme
+;;     :ensure t
+;;     :config
+;;     (iceberg-theme-create-theme-file)
+;;     (load-theme "solarized-iceberg-dark" t))
+
 
 ;;; Code:
 
